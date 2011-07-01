88027f17f4614166ddcb334a55c684dd6586d3dc
Hugo Miranda <hugo@aioninnovations.com>
2011-07-01 01:07:49 -0300

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminAion::Application.config.secret_token = 'a109dd8620f9c242b6b2b90088de31f520f0577e575f2bb1a33f50431d603e54354b3ecb69b239e59d6aac61ca5e5b06db179b923183a8889fb43e3e4377eaeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AdminAion::Application.config.session_store :cookie_store, :key => '_admin_aion_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# AdminAion::Application.config.session_store :active_record_store
